db3aa9c409dec39232e5c170b617139475103a25
快乐的老鼠宝宝 <keaitianxinmail@qq.com>
2023-06-24 02:40:17 +0800

    统计出仓库内主线文本与各contributor编辑过的
    看条数多少
    
    ```
    PS D:\Git\MozillaChina\common-voice-fix> & C:/Environment/Python/Python311/python.exe d:/Git/MozillaChina/common-voice-fix/src/analyse/de-duplicate.py
    cn.txt origin 3182 YF 3182
    sentence-collector.txt origin 267 YF 541
    wiki.zh-cn.txt origin 47641 YF 46605
    ```

--- src/analyse/de-duplicate.py
+++ src/analyse/de-duplicate.py
@@ -8,8 +8,12 @@ corpus_list=list(filter(bool,[i if os.path.isfile(os.path.join(PATH,i)) else "" 
 contributor_list=list(filter(bool,[i if os.path.isdir(os.path.join(PATH,i)) else "" for i in os.listdir(PATH)]))
 
 
-def de_duplicate():
-    pass
+def de_duplicate(i,contributor):
+    origin_file=open(os.path.join(PATH,i),"r",encoding="utf-8")
+    origin_content=origin_file.readlines()
+    contributor_file=open(os.path.join(PATH,contributor,i),"r",encoding="utf-8")
+    contributor_content=contributor_file.readlines()
+    print(i,"origin",len(origin_content),contributor,len(contributor_content))
 
 for contributor in contributor_list:
     temp_corpus_list=os.listdir(os.path.join(PATH,contributor))
